b016b0164d7a7cd64a0c6b54a28ff33a963d3126
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-02-26 09:31:02 +0200

    Update

---
+++ project/trainer.py
@@ -0,0 +1,31 @@
+from typing import List
+from project.pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon in self.pokemons:
+            return "This pokemon is already caught"
+
+        self.pokemons.append(pokemon)
+        return f"Caught {pokemon.pokemon_details()}"
+
+    def release_pokemon(self, pokemon_name):
+        try:
+            pokemon = [p for p in self.pokemons if p.name == pokemon_name][0]
+        except IndexError:
+            return 'Pokemon is not caught'
+
+        self.pokemons.remove(pokemon)
+        return f"You have released {pokemon_name}"
+
+    def trainer_data(self):
+        result = '\n'.join(f'- {p.pokemon_details()}' for p in self.pokemons)
+
+        return f"Pokemon Trainer {self.name}:\n" \
+               f"Pokemon count {len(self.pokemons)}\n" \
+               f'{result}'
