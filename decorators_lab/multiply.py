d989935a1c94d93043f6f547150d054e9046c03c
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-03-27 10:31:23 +0200

    Update

---
+++ decorators_lab/multiply.py
@@ -0,0 +1,14 @@
+def multiply(num):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
+            return result * num
+        return wrapper
+    return decorator
+
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+print(add_ten(6))
